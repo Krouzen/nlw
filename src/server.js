5deec7547b190a431db8715d8de747a9201f3d87
Gabriel <gabrielbelo@hotmail.com>
2020-06-05 18:08:03 -0300

    organização das páginas

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const server = express()
+server.use(express.static("public"))
+server.get("/", (req, res) => {
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+server.listen(3000)
